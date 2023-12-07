d30bddd0cce4d6c66a202a50bf831c165a9771d5
Carlos Ortiz <carlosortiz@192.168.0.7>
2023-12-06 21:24:26 -0500

    changing plots

--- app.py
+++ app.py
@@ -6,23 +6,31 @@ st.title("Esto es una app")
 
 X = np.random.normal(0, 1, 1000)
 Y = np.random.normal(0, 5, 1000)
+e = np.random.normal(0, 1, 1000)
 
 tab1, tab2 = st.tabs(["Tab1", "Tab2"])
 
 with tab1:
-    fig, ax = plt.subplots(1, 1)
-    ax.hist(X, bins=50, alpha=0.5, color='g')
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
+    fig, ax = plt.subplots(1, 2, sharey=True)
+    ax[0].hist(X, bins=50, alpha=0.5, color='g')
+    ax[0].spines['right'].set_visible(False)
+    ax[0].spines['top'].set_visible(False)
+    ax[0].axvline(X.mean(), color='g')
+
+    ax[1].hist(Y, bins=50, alpha=0.5, color='g')
+    ax[1].spines['right'].set_visible(False)
+    ax[1].spines['top'].set_visible(False)
+    ax[1].axvline(Y.mean(), color='g')
 
     st.pyplot(fig)
 
 with tab2:
 
     fig, ax = plt.subplots(1, 1)
-    ax.hist(Y, bins=50, alpha=0.5, color='g')
+    ax.plot(e, color='k', alpha=0.6, ls='--')
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
+    ax.axvline(e.mean(), color='k')
 
     st.pyplot(fig)
 
